21a55c2367eebf2c64cde5c84357331da6c14e91
ASUS <nsk-pto@mail.ru>
2020-02-11 22:30:21 +0700

    Добавьте файлы проекта.

---
+++ BeginC/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeginC
+{
+    class Class1
+    {
+    }
+}
---
+++ BeginC/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BeginC
+{
+    class Program
+    {
+        public static void print(int number)
+        {
+            Console.WriteLine(number);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program.print(55);
+        }
+    }
+}
