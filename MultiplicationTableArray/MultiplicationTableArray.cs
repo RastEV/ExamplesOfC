64ab4033941d1fda45fba3b56b7e59a8644fdac0
ASUS <nsk-pto@mail.ru>
2020-02-18 21:37:28 +0700

    MT

---
+++ MultiplicationTableArray/MultiplicationTableArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MultiplicationTableArray
+{
+    class MultiplicationTableArray
+    {
+        public static void Print(int[,] Array)
+        {
+            for (int i = 0; i < Array.GetLength(0); ++i)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < Array.GetLength(1); ++j)
+                {
+                    Console.Write("{0,3}", Array[i,j]);
+                }
+            }
+        }
+
+        public static int[,] GetMultiplicationTable(int size1, int size2)
+        {
+            int[,] multiplicationTable = new int[size1, size2];
+            for (int i = 0; i < size1; ++i)
+            {
+                for (int j = 0; j < size2; ++j)
+                {
+                    multiplicationTable[i,j] = (i + 1) * (j + 1);
+                }
+            }
+            return multiplicationTable;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Первый множитель");
+            int size1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Второй множитель");
+            int size2 = Convert.ToInt32(Console.ReadLine());
+
+            Print(GetMultiplicationTable(size1, size2));
+
+        }
+    }
+}
